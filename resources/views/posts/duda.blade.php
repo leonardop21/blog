0efb452be1ad6c0790d5f6f0cd4ea70eb85376e5
Leonardo Nascimento <leonardo@14bisdigital.com.br>
2016-05-05 16:31:16 -0300

    Duda

--- resources/views/posts/duda.blade.php
+++ resources/views/posts/duda.blade.php
@@ -1,21 +1,23 @@
 @extends('template.section')
 @section('title') Zoando o Betoneira Plus +++ @stop
-
-@section('content') 
+@section('content')
 <div class="text-center"><h1> Zoando o Duda</h1></div>
 <div class="container">
 	<div class="row">
 	<?php foreach ($dudas as $key => $duda): ?>
-			<div class="col-md-6">
+			<div class="col-md-4">
 				<h3><?php echo $duda['title']; ?></h3>
 				<p>
 					<?php echo $duda['content']; ?>
 				<a href="<?php echo $duda['img']; ?>" target="_blank">
-					<img class="img-responsive" src="<?php echo $duda['img']; ?>" width="350" height="300">
+					<img class="duda img-responsive" src="<?php echo $duda['img']; ?>">
 				</a>
 				</p>
 			</div>
 	<?php endforeach; ?>
 	</div>
 </div>
+<style type="text/css">
+.duda {width: 300px; height:350px; align-items: right;}
+</style>
  @stop
